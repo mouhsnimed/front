e971b254d6caa2653d8d5f0081f1bdcf1ea592d1
mohamed mouhsni <m.mouhsni@gmail.com>
2021-06-19 23:37:38 +0100

    add template to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/style.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./home";
+import About from "./about";
+import Details from "./details";
+import Listing from "./listing";
+import Header from "./shared/Header";
+import Footer from "./shared/Footer";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/listing" exact>
+            <Listing />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+          <Route path="/details" exact>
+            <Details />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/shared/Header.js
@@ -0,0 +1,144 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header id="site-header" className="fixed-top">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg stroke px-0">
+          <h1>
+            <Link className="navbar-brand" to="index.html">
+              <span className="fa fa-home"></span> Estate sale
+            </Link>
+          </h1>
+
+          {/* <Link className="navbar-brand" to="#index.html">
+      <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+  </Link> */}
+          <button
+            className="navbar-toggler  collapsed bg-gradient"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon fa icon-expand fa-bars"></span>
+            <span className="navbar-toggler-icon fa icon-close fa-times"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-lg-5 mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="index.html">
+                  Home <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item @@listing__active">
+                <Link className="nav-link" to="/listing">
+                  Listing
+                </Link>
+              </li>
+              <li className="nav-item @@listing__active">
+                <Link className="nav-link" to="/details">
+                  Details
+                </Link>
+              </li>
+              <li className="nav-item @@listing__active">
+                <Link className="nav-link" to="/about-us">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item @@listing__active">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              {/* <li className="nav-item dropdown @@property__active">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Property <span className="fa fa-angle-down"></span>
+                </Link>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Link
+                    className="dropdown-item @@ps__active"
+                    to="property-single.html"
+                  >
+                    Single property
+                  </Link>
+                </div>
+              </li> */}
+              {/* <li className="nav-item dropdown @@pages__active">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Pages <span className="fa fa-angle-down"></span>
+                </Link>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Link
+                    className="dropdown-item @@about__active"
+                    to="about.html"
+                  >
+                    About us
+                  </Link>
+                  <Link
+                    className="dropdown-item @@contact__active"
+                    to="contact.html"
+                  >
+                    Contact us
+                  </Link>
+                </div>
+              </li> */}
+            </ul>
+            <div className="top-quote mt-lg-0">
+              <Link to="#buytheme" className="btn btn-style btn-primary">
+                <span className="fa fa-home"></span> Add listiing
+              </Link>
+            </div>
+
+            <div className="search mx-3">
+              <input className="search_box" type="checkbox" id="search_box" />
+              <label className="fa fa-search" for="search_box"></label>
+              <div className="search_form">
+                <form action="error.html" method="GET">
+                  <input type="text" placeholder="Search" />
+                  <input type="submit" value="search" />
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="mobile-position">
+            <nav className="navigation">
+              <div className="theme-switch-wrapper">
+                <label className="theme-switch" for="checkbox">
+                  <input type="checkbox" id="checkbox" />
+                  <div className="mode-container">
+                    <i className="gg-sun"></i>
+                    <i className="gg-moon"></i>
+                  </div>
+                </label>
+              </div>
+            </nav>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
