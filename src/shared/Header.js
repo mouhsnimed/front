35f6db23951a5f6e67da35e01b99e12a3f16a990
Romaric Kwakou <romaric.kwakou@gmail.com>
2021-07-16 10:01:37 +0100

    gestion du listing et de la recherche ainsi que certaine traduction du UI

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -8,6 +8,7 @@ import { useHistory } from "react-router-dom";
 const Header = () => {
   const history = useHistory();
   const [isFixed, setIsFixed] = useState(false);
+  const [current, setCurrent] = useState(0);
   const handleOnscroll = () => {
     setIsFixed(window.scrollY >= 80);
   };
@@ -50,14 +51,17 @@ const Header = () => {
       <div className="container">
         <nav className="navbar navbar-expand-lg stroke px-0">
           <h1>
-            <Link className="navbar-brand" to="/">
-              <span className="fa fa-home"></span> Estate sale
+            <Link className="navbar-brand" to="/"  style={{textTransform:"none"}}>
+              {
+                //<span className="fa fa-bank"></span
+              }
+              <b className="badge badge-primary" style={{color:"white",fontSize:"30px"}} >A</b>rt-ppart
             </Link>
           </h1>
 
           {/* <Link className="navbar-brand" to="#index.html">
-      <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
-  </Link> */}
+              <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+          </Link> */}
           <button
             className="navbar-toggler  collapsed bg-gradient"
             type="button"
@@ -73,28 +77,28 @@ const Header = () => {
 
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-lg-5 mr-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                  Home <span className="sr-only">(current)</span>
+              <li className={`nav-item ${current == 0 && "active" }`}>
+                <Link className="nav-link" to="/" onClick={() => (setCurrent(0))}>
+                  Accueil 
                 </Link>
               </li>
-              <li className="nav-item @@listing__active">
-                <Link className="nav-link" to="/listing">
-                  Listing
+              <li className={`nav-item ${current == 1 && "active" }`}>
+                <Link className="nav-link" to="/listing" onClick={() => (setCurrent(1))}>
+                  Annonces 
                 </Link>
               </li>
-              <li className="nav-item @@listing__active">
+              {/* <li className="nav-item @@listing__active">
                 <Link className="nav-link" to="/details">
                   Details
                 </Link>
-              </li>
-              <li className="nav-item @@listing__active">
-                <Link className="nav-link" to="/about-us">
-                  About
+              </li> */}
+              <li className={`nav-item ${current == 2 && "active" }`}>
+                <Link className="nav-link" to="/about-us" onClick={() => (setCurrent(2))}>
+                 Nous ?
                 </Link>
               </li>
-              <li className="nav-item @@listing__active">
-                <Link className="nav-link" to="/contact">
+              <li className={`nav-item ${current == 3 && "active" }`}>
+                <Link className="nav-link" to="/contact" onClick={() => (setCurrent(3))}>
                   Contact
                 </Link>
               </li>
